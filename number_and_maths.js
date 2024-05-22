9f8d68002e51e1459ea8f82938fac767b02ffeef
Amaan909 <amaansid909@gmail.com>
2024-05-22 19:10:40 +0530

    Math and number

--- number_and_maths.js
+++ number_and_maths.js
@@ -1,7 +1,28 @@
 const num=new Number(100)//check prototypes
 console.log(num);
-console.log(num.toString());
-console.log(num.toFixed(2));//100.00
+//console.log(num.toString());
+//console.log(num.toFixed(2));//100.00
+
+//Maths
+
+// console.log(Math);//check for prototypes
+// console.log(Math.min(4,3,2,1));
+console.log(Math.random());//between 0,1
+console.log(Math.random()*10);//between 0,9
+console.log((Math.random()*10)+1);//between 1,9
+console.log(Math.floor(Math.random()*10+1));//integer
+
+const min=100
+const max=200;
+console.log(Math.floor(((Math.random()*(max-min+1))+min)));
+
+
+
+
+
+
+
+
 
 
 
